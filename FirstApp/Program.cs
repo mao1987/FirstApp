be4c66e7250f92844f86c8dfda776ffeab5a98a7
mao1987 <mao_2005@mail.ru>
2021-10-31 00:17:48 +0300

    Добавьте файлы проекта.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Jane";
+            Console.WriteLine(MyName);
+
+            Console.ReadKey();
+        }
+    }
+}
